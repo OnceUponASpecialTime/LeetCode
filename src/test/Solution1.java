0849ff202489494a5ec74ef1a2cfad91d607a51b
Foutas <100391764@qq.com>
2017-05-17 20:02:52 +0800

    0517.leetcode

--- src/test/Solution1.java
+++ src/test/Solution1.java
@@ -216,4 +216,24 @@ public class Solution1 {
         }
         return res;
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {val = x;}
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSame(s.left, t) || isSame(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
 }
